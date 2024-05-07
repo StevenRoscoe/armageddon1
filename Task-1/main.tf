bacc84c9948f5ca328b0975c7742465fcafb92a4
StevenRoscoe <dbzroscoe@gmail.com>
2024-05-06 21:38:04 -0500

    working on bucket

--- Task-1/main.tf
+++ Task-1/main.tf
@@ -1,14 +1,29 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.28.0"
     }
   }
 }
 
 provider "google" {
-  project = ""
-  region = ""
-  zone = ""
+  project     = "terraform-mania-1997"
+  region      = "us-central1"
+  zone        = "us-central-a"
+  credentials = "terraform-mania-1997-96587c92223a.json"
 }
+
+resource "google_storage_bucket" "sroscoe_public_bucket" {
+  name          = "sroscoe-terraform-bucket-task1"
+  location      = "US-CENTRAL1"
+  force_destroy = true
+
+  public_access_prevention = "inherited"
+}
+
+#resource "google_storage_bucket_access_control" "public_rule" {
+  #bucket = google_storage_bucket.sroscoe_terraform_bucket_task1
+  #role   = "READER"
+  #entity = "allUsers"
+#}
